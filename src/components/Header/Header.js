979b975beb52cf3686b15a3cf93e7b32cfc077a5
Ciara Bujanos <c.buj22@gmail.com>
2018-02-19 09:03:54 -0700

    initial commit, header rendering

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <ul>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
