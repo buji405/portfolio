8fc65d3e4f644cf30b107d561b8e3b2d7faa8083
Ciara Bujanos <c.buj22@gmail.com>
2018-02-19 21:30:46 -0700

    animation with keyframs in intro

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -4,7 +4,15 @@ import './introduction.css';
 const Introduction = () => {
   return (
     <section id="introduction">
-      <article className="self-portrait"></article>
+      <article className="self-portrait">
+        <h1>Front-End Developer</h1>
+        <div className="animation-words">
+          <span>Pixel Perfectionist</span>
+          <span>Builder of Modular Components</span>
+          <span>Responsive Advocate</span>
+          <span>Yes to Semicolons</span>
+        </div>
+      </article>
     </section>
   )
 }
