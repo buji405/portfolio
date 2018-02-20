95ab3cacedf6ce57f321fc944cf48cba251ffc92
Ciara Bujanos <c.buj22@gmail.com>
2018-02-19 17:01:41 -0700

    avatar image rendering

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import Introduction from './components/Introduction/Introduction';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Introduction />
         <About />
       </div>
     );
---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './introduction.css';
+
+const Introduction = () => {
+  return (
+    <section id="introduction">
+      <article className="self-portrait"></article>
+    </section>
+  )
+}
+
+export default Introduction;
